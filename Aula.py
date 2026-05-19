ad2c7010323869de20f55673fb338379c29e4440
Lucas <astombb@gmail.com>
2026-05-19 17:24:37 -0300

    teste 2

--- Aula.py
+++ Aula.py
@@ -1,1 +1,15 @@
-print("Hello World")
+def formulario( ):
+
+    nome = input("digite seu nome: ")
+    idade = int(input("digite sua idade: "))
+    sexo = input("digite seu sexo: ")
+    profissao = input("digite sua profissão: ")
+
+    
+        
+    print(f"\nformulario preenchido com sucesso!\n")
+    return f'Nome: {nome}\nIdade: {idade}\nSexo: {sexo}\nProfição: {profissao}'
+
+
+
+print(formulario())
