aa7bb1915bee1a6e5770a3b62c5ac73df20b3e2e
Lucas <astombb@gmail.com>
2026-05-19 21:14:36 -0300

    adicao arquivo

---
+++ condicional.py
@@ -0,0 +1,15 @@
+idade = int(input("Qual é a sua idade?: "))
+
+if idade >= 18:
+    print ("Autorizado")
+else:
+    print("Não autorizado")
+    
+senha = "Lrs12345"    
+digitasenha = input("Digite sua senha:") 
+
+if digitasenha == {senha}:
+    print("Acesso permitido")
+else:
+    digitasenha != {senha}
+print("acesso Negado")
